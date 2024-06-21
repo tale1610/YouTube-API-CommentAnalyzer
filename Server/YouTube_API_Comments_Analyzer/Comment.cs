e22ad5f3d51a0ba57697f72c84cb1153704f8824
taskov_ <taskovvukan@gmail.com>
2024-06-21 20:21:54 +0200

    Initial commit

---
+++ Server/YouTube_API_Comments_Analyzer/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTube_API_Comments_Analyzer;
+
+public class Comment
+{
+    public string AuthorDisplayName { get; set; }
+    public string TextDisplay { get; set; }
+    public int LikeCount { get; set; }
+    public DateTime PublishedAt { get; set; }
+}
